00543fd5c48e16670cdf30efdb841de11211e3a9
Scott Silverman <scottms724@gmail.com>
2018-04-02 19:08:57 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   newNumbers = []
   array.each do |number|
-    numberSquared = number.to_i**2
+    numberSquared = number**2
     newNumbers.push(numberSquared)
   end
 end
